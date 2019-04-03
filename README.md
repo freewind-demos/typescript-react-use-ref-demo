da7c414e9641e14324fb3b76fcc7e93a46205b45
freewind <nowindlee@gmail.com>
2019-04-03 17:14:49 +0800

    demo

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-TypeScript React Hello World Demo
+TypeScript React "useRef" Demo
 =================================
+
+使用`useRef`可以让我们使用const来持有一个可能会发生变化的`ref`，并使用`.current`拿到当前最新的element
 
 ```
 npm install
 npm run demo
 ```
-
-It will open page on browser automatically.
--- src/hello.tsx
+++ src/hello.tsx
@@ -1,8 +1,20 @@
 //import * as React from 'react'
-import React from 'react'
+import React, {useRef, useState, useEffect} from 'react'
 
 export default function Hello() {
+  const [value, setValue] = useState('')
+  const messageRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const content = messageRef.current!.textContent;
+    console.log(content)
+  }, [value])
+
   return <div>
     <h1>Hello React</h1>
+    <div>
+      <input type='text' value={value} onChange={event => setValue(event.currentTarget.value)}/>
+      <div ref={messageRef}>Hello, {value}</div>
+    </div>
   </div>
 };
